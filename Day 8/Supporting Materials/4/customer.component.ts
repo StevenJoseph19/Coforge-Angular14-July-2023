15ee0fa9f0d7910eb72abe62d3624e1c74d58df3
Stephen Samuels <stevesam1927@gmail.com>
2023-07-20 22:37:28 +0530

    added Day 8 supporting materials

---
+++ Day 8/Supporting Materials/4/customer.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+  customer = new Customer();
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true
+    });
+  }
+
+  populateTestData(): void {
+    this.customerForm.setValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+}
